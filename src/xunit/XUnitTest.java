d9cb34a6ce56ed2470b61f5fa67e9855327dedd1
최재원/Edu팀/TO <jae.won.choi@nhnsoft.com>
2022-09-05 14:36:57 +0900

    commit E-1

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -2,7 +2,6 @@ package xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        System.out.println("TestStart");
         TestResult result = new TestResult();
         TestSuite suite = TestCaseTest.suite();
         suite.run(result);
